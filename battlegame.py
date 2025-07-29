d4abb8321600bb5ac93a3ac59a558062105732dd
Arvin Greenberg Graff <arvingreenberggraff@ellas-mbp.lan>
2025-07-29 21:21:26 +0200

    Make battle mode characters look like mafia members with suits, hats, and mustaches

--- battlegame.py
+++ battlegame.py
@@ -401,20 +401,20 @@ while True:
 
     # Draw characters differently based on mode
     if mode == 0:
-        # Battle mode: mafia style
+        # Battle mode: mafia style with sunglasses and non-happy smile
         # Player 1
-        # Head (skin tone)
         pygame.draw.ellipse(screen, (224, 172, 105), (player1.centerx-15, player1.centery-35, 30, 38))
         # Fedora hat
         pygame.draw.rect(screen, (60,60,60), (player1.centerx-15, player1.centery-45, 30, 10))
         pygame.draw.rect(screen, (0,0,0), (player1.centerx-10, player1.centery-50, 20, 8))
-        # Eyes (serious)
-        pygame.draw.ellipse(screen, (255,255,255), (player1.centerx-8, player1.centery-25, 7, 10))
-        pygame.draw.ellipse(screen, (255,255,255), (player1.centerx+1, player1.centery-25, 7, 10))
-        pygame.draw.ellipse(screen, (0,0,0), (player1.centerx-5, player1.centery-21, 3, 5))
-        pygame.draw.ellipse(screen, (0,0,0), (player1.centerx+4, player1.centery-21, 3, 5))
+        # Sunglasses
+        pygame.draw.rect(screen, (0,0,0), (player1.centerx-12, player1.centery-28, 24, 8))
+        pygame.draw.rect(screen, (30,144,255), (player1.centerx-12, player1.centery-28, 24, 8), 2)
+        # Eyes hidden by sunglasses
         # Mustache
         pygame.draw.arc(screen, (80,42,20), (player1.centerx-7, player1.centery-8, 14, 6), 3.14, 2*3.14, 2)
+        # Non-happy smile (flat or slight frown)
+        pygame.draw.arc(screen, (0,0,0), (player1.centerx-7, player1.centery-4, 14, 6), 3.14, 2*3.14, 2)
         # Suit (black jacket, white shirt, red tie)
         pygame.draw.rect(screen, (0,0,0), (player1.centerx-14, player1.centery+3, 28, 38))
         pygame.draw.rect(screen, (255,255,255), (player1.centerx-6, player1.centery+3, 12, 38))
@@ -434,11 +434,11 @@ while True:
         pygame.draw.ellipse(screen, (224, 172, 105), (player2.centerx-15, player2.centery-35, 30, 38))
         pygame.draw.rect(screen, (60,60,60), (player2.centerx-15, player2.centery-45, 30, 10))
         pygame.draw.rect(screen, (0,0,0), (player2.centerx-10, player2.centery-50, 20, 8))
-        pygame.draw.ellipse(screen, (255,255,255), (player2.centerx-8, player2.centery-25, 7, 10))
-        pygame.draw.ellipse(screen, (255,255,255), (player2.centerx+1, player2.centery-25, 7, 10))
-        pygame.draw.ellipse(screen, (0,0,0), (player2.centerx-5, player2.centery-21, 3, 5))
-        pygame.draw.ellipse(screen, (0,0,0), (player2.centerx+4, player2.centery-21, 3, 5))
+        pygame.draw.rect(screen, (0,0,0), (player2.centerx-12, player2.centery-28, 24, 8))
+        pygame.draw.rect(screen, (255,0,0), (player2.centerx-12, player2.centery-28, 24, 8), 2)
+        # Eyes hidden by sunglasses
         pygame.draw.arc(screen, (80,42,20), (player2.centerx-7, player2.centery-8, 14, 6), 3.14, 2*3.14, 2)
+        pygame.draw.arc(screen, (0,0,0), (player2.centerx-7, player2.centery-4, 14, 6), 3.14, 2*3.14, 2)
         pygame.draw.rect(screen, (0,0,0), (player2.centerx-14, player2.centery+3, 28, 38))
         pygame.draw.rect(screen, (255,255,255), (player2.centerx-6, player2.centery+3, 12, 38))
         pygame.draw.rect(screen, (200,0,0), (player2.centerx-2, player2.centery+10, 4, 24))
